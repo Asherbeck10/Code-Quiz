5bd291d053f70b49225a2a58c0f341660611ba6c
Asherbeck10 <asher.bec@gmail.com>
2023-01-14 07:56:00 +0000

    check point

--- starter/assets/js/logic.js
+++ starter/assets/js/logic.js
@@ -16,16 +16,18 @@ divQuestion.classList.remove("hide")
 // Array of functions
 let questionsArray=Object.keys(questions);
 
-let i=0
+
 
 
 for (let i = 0; i < questionsArray.length; i++) {
-    let rightAnswer=window[questionsArray[i]]();
-    questionChoices.addEventListener("click",function(event){
+        let rightAnswer=window[questionsArray[i]]();
+        questionChoices.addEventListener("click",function(event){
         if (event.target.matches("button")) {
             let buttonIndex = event.target.getAttribute("data-index");
-            let=userAnswer=questionChoices.children[buttonIndex].textContent
+            userAnswer=questionChoices.children[buttonIndex].textContent
+            console.log(userAnswer)
              }
+            
         if (rightAnswer===userAnswer) {
             console.log("well done")
         }else{
@@ -38,11 +40,3 @@ for (let i = 0; i < questionsArray.length; i++) {
 
 
 
-function answers()
-{let userAnswer = event.target.getAttribute("data-index")
-    console.log(userAnswer)
-    console.log(rightAnswer)
-     if (userAnswer===rightAnswer) {
-    console.log("well done") 
-     }else{console.log("wrong answer")};
-    }
